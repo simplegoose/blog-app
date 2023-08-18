43a9773a38a4d65db1f9513b7dce06aa135c6cd1
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-18 23:37:57 +0300

    Lint files

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def get_recent_comments
+  def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  def get_recent_posts
+  def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
