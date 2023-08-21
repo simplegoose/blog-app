b4aa117f9db13ae13a3b3e4a272a69812ba794c5
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-21 22:30:00 +0300

    Lint files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  #GET /users/1/posts
+  # GET /users/1/posts
   def posts
     @user = User.find(params[:user_id])
   end
 
-  #GET /users/1/posts/10
+  # GET /users/1/posts/10
   def show_post_by_id
     @post = Post.find(params[:post_id])
     @user = User.find(params[:user_id])
