52aaeffb3331465359ef703ffbabfb9cce62f98f
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-21 22:19:29 +0300

    Add routes for newly created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
+  # Defines the root path route ("/")
+
+  root "users#index"
+  resources :users, only: [:show]
+  get "users/:user_id/posts", to: "users#posts"
+  get "users/:user_id/posts/:post_id", to: "users#show_post_by_id"
 end
