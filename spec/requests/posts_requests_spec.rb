fdb1f9bc2ecfba4a556b9617c76bc815360f1bb5
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-23 15:05:29 +0300

    Describe specs for index and show routes

---
+++ spec/requests/posts_requests_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  
+  describe "GET /index" do
+    let(:user) { User.create(name: 'Kane', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Footballer from England') }
+    
+    it "returns /users/:user_id/posts page view as html" do
+      get "/users/#{user.id}/posts"
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index) 
+    end
+  end
+
+  describe "GET /show" do
+    let(:user) { User.create(name: 'Kane', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Footballer from England') }
+    let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
+
+    it "returns /users/:user_id/posts/:post_id page view as html" do
+      get "/users/#{user.id}/posts/#{post.id}"
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show) 
+    end
+  end
+  
+end
