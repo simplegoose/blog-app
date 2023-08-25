49e417a83b23862cd9811e377c649a76b5753a99
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-25 13:42:20 +0300

    New method that returns current user Create route controller that returns post form content Create a route for handling post request for new post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,37 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def new
+    post = Post.create
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    user = current_user
+    post = Post.new(params.require(:post).permit(:title, :text))
+
+    post.author = user
+    if post.save
+      flash[:success] = "Success: Your post has been saved" 
+      puts 'user_path heere'
+      redirect_to users_home_path
+    else
+      post = Post.create
+      flash[:error] = "Error: Post could not be saved"
+      render :new, locals: { post: post }
+    end
+  end
+
   # GET /users/1/posts/10
   def show
     @post = Post.find(params[:post_id])
     @user = User.find(params[:user_id])
   end
+
+  def current_user
+    @user = User.first
+  end
+  
 end
