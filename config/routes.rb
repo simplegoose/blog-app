99a045972feb732ccc4066cf731ae70ef7f3ed82
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-25 22:17:25 +0300

    Add routes to comment create

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   get "users/:user_id/posts", to: "posts#index"
   get "users/:user_id/posts/:post_id", to: "posts#show", as: "posts_show"
+
   get "post/new", to: "posts#new", as: "new_post" # new
   post "post/create", to: "posts#create", as: "post_create" #create
+
   post "like/create", to: "likes#create", as: "like_create" #create
+
+  get "users/:user_id/post/:post_id/comment/new", to: "comments#new" #new
+  post "comment/create", to: "comments#create", as: "comment_create" #create
+
 end
