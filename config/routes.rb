a7d7970b40cae70d64591607e80c3ce6b9f32df5
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-08-25 14:19:15 +0300

    Add new routes for likes creation, post creation and post form view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   # Defines the root path route ("/")
 
-  root "users#index"
+  root "users#index", as: "users_home"
   resources :users, only: [:show]
   get "users/:user_id/posts", to: "posts#index"
-  get "users/:user_id/posts/:post_id", to: "posts#show"
+  get "users/:user_id/posts/:post_id", to: "posts#show", as: "posts_show"
+  get "post/new", to: "posts#new", as: "new_post" # new
+  post "post/create", to: "posts#create", as: "post_create" #create
+  post "like/create", to: "likes#create", as: "like_create" #create
 end
